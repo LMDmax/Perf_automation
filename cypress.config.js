26c5941c429812bb2a309387bb4b6c6b3f714bed
gaurav-softsensor <97886891+gaurav-softsensor@users.noreply.github.com>
2025-08-12 13:47:17 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,20 @@
 const { defineConfig } = require("cypress");
+
 module.exports = defineConfig({
   projectId: 'j7i5bp',
   e2e: {
     baseUrl: 'https://staging.performance.lmdmax.com/',
-    downloadsFolder: "cypress/downloads", // Specify the downloads folder
+    downloadsFolder: "cypress/downloads",
+    experimentalStudio: true,
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: false,
+      json: true
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-    experimentalStudio: true,  //enable cypress studio
+    }
   },
 });
