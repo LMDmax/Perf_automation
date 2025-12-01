4a8f5209e7c77abf217b7987e28c1a2705e8d958
priyanka-sharma-24 <priyanka.sharma@softsensor.ai>
2025-12-01 07:55:29 -0500

    test remove

--- cypress/e2e/API testing/test.cy.js
+++ cypress/e2e/API testing/test.cy.js
@@ -1,40 +1,40 @@
-import Login from "../../Pageobjects/Loginpage.JS";
-import dailyrepo from "../../Pageobjects/Daily report.js";
-import Settings from "../../Pageobjects/Setting.js";
-import Profile from "../../Pageobjects/Profile.js";
-import dashboard from "../../Pageobjects/Dashboard.js";
-import driver from "../../Pageobjects/Driver.js"; 
-import Chat from "../../Pageobjects/Chat.js";
-import acknowledgment from "../../Pageobjects/acknowledgement.js";
+// import Login from "../../Pageobjects/Loginpage.JS";
+// import dailyrepo from "../../Pageobjects/Daily report.js";
+// import Settings from "../../Pageobjects/Setting.js";
+// import Profile from "../../Pageobjects/Profile.js";
+// import dashboard from "../../Pageobjects/Dashboard.js";
+// import driver from "../../Pageobjects/Driver.js"; 
+// import Chat from "../../Pageobjects/Chat.js";
+// import acknowledgment from "../../Pageobjects/acknowledgement.js";
 
-describe('Login page', () => {
-  before(() => {
-    Cypress.config('defaultCommandTimeout', 10000);
-  }); 
+// describe('Login page', () => {
+//   before(() => {
+//     Cypress.config('defaultCommandTimeout', 10000);
+//   }); 
 
-it('LoginTest', () => {
-  cy.visit('https://staging.performance.lmdmax.com')
+// it('LoginTest', () => {
+//   cy.visit('https://staging.performance.lmdmax.com')
 
-  //first create object of class
-  const ln=new Login();
-  ln.setUserName('allchat575@gmail.com');
-  ln.setPassword('Test@1234');
-  ln.Clickbutton();
+//   //first create object of class
+//   const ln=new Login();
+//   ln.setUserName('allchat575@gmail.com');
+//   ln.setPassword('Test@1234');
+//   ln.Clickbutton();
 
- //cy.get('input[type="checkbox"].PrivateSwitchBase-input').click();
+//  //cy.get('input[type="checkbox"].PrivateSwitchBase-input').click();
 
-    //const dash = new dashboard()
-      //dash.Dailyhigh();
+//     //const dash = new dashboard()
+//       //dash.Dailyhigh();
 
-      //const dr=new driver()
-      //dr.driverdash();
+//       //const dr=new driver()
+//       //dr.driverdash();
 
-      const ch=new Chat()
-        ch.chattwillio();
+//       const ch=new Chat()
+//         ch.chattwillio();
 
-      // const ack=new acknowledgment()
-      //  ack.getacknowledgment();
-      //  ack.addacknowledgment();
-  })
+//       // const ack=new acknowledgment()
+//       //  ack.getacknowledgment();
+//       //  ack.addacknowledgment();
+//   })
 
-})
+// })
